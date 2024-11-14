c1b3969db858421bfa3ccc6377c0b2064ccea09f
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2024-11-14 10:45:22 +0200

    feat: add account creation with OTP verification in AuthForm
    - Implement `onSubmit` handler to create a new account with full name and email
    - Set loading state and display error message if account creation fails
    - Store `accountId` upon successful account creation

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,6 +16,7 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { createAccount } from "@/lib/actions/user.actions";
 
 type FormType = "sign-in" | "sign-up";
 const authFormSchema = (formType: FormType) => {
@@ -32,6 +33,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const formSchema = authFormSchema(type);
+  const [accountId, setAccountId] = useState(null);
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -44,7 +46,20 @@ const AuthForm = ({ type }: { type: FormType }) => {
 
   // 2. Define a submit handler.
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const user = await createAccount({
+        fullName: values.fullName || "",
+        email: values.email,
+      });
+      setAccountId(user.accountId);
+    } catch {
+      setErrorMessage("Failed to create account. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
   };
   return (
     <>

ad19e1f82352bf42a77a78c489eeb481a5b43e67
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2024-11-14 10:31:59 +0200

    feat: implement account creation flow with OTP verification
    - Add `getUserByEmail` function to check if a user exists in the database by email
    - Create `sendEmailOTP` function to send OTP for email verification
    - Implement `createAccount` function to:
      - Check if the user exists by email
      - Send OTP and retrieve account ID
      - Create a new user document with full name, email, avatar placeholder, and account ID if user is new
    - Include error handling to log and rethrow errors

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,67 @@
+"use server";
 // **Create account flow**
 // 1. User enters full name and email
 // 2. Check if the user already exist using the email (we will use to identify if we still need to create a user document or not)
 // 3. Send OTP to user’s email
-// 1. This will send a secret key for creating a session. The secret key
-// 4. Create a new user document if the user is a new user
+// 4. This will send a secret key for creating a session. The secret key
+// 5. Create a new user document if the user is a new user
 // 5. Return the user’s accountId that will be used to complete the log
-// 6. Verify OTP and authenticate to login
+// 7. Verify OTP and authenticate to login
+
+import { createAdminClient } from "@/lib/appwrite";
+import { appWriteConfig } from "@/lib/appwrite/config";
+import { ID, Query } from "node-appwrite";
+import { parseStringify } from "@/lib/utils";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+  const result = await databases.listDocuments(
+    appWriteConfig.databaseId,
+    appWriteConfig.usersCollectionId,
+    [Query.equal("email", [email])],
+  );
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = getUserByEmail(email);
+  const accountId = await sendEmailOTP({ email });
+  if (!accountId) throw new Error("Failed to send an OTP");
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+    await databases.createDocument(
+      appWriteConfig.databaseId,
+      appWriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://in.pinterest.com/pin/placeholder-clipart-png-images-user-avatar-placeholder-white-blue-profile-picture-placeholder-white-png-image-for-free-download--727331408586770494/",
+        accountId,
+      },
+    );
+  }
+  return parseStringify({ accountId });
+};

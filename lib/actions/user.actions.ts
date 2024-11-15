90ef7cdd008a36531a869c2983176d2a34965e7f
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2024-11-15 12:46:35 +0200

    Sign in function implemented

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -126,3 +126,17 @@ export const signOutUser = async () => {
     redirect("/sign-in");
   }
 };
+
+export const signInUser = async ({ email }: { email: string }) => {
+  try {
+    const existingUser = await getUserByEmail(email);
+    //user exist send OTP
+    if (existingUser) {
+      await sendEmailOTP({ email });
+      return parseStringify({ accountId: existingUser.accountId });
+    }
+    return parseStringify({ accountId: null, error: "User not found" });
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};

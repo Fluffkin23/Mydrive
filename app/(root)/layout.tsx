1150dc0bb668ca587f512471e492c6680b57a0bf
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2024-11-16 21:31:05 +0200

    Adding a max size upload minim with toast message

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
+import { Toaster } from "@/components/ui/toaster";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const currentUser = await getCurrentUser();
@@ -17,6 +18,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
         <Header />
         <div className="main-content">{children}</div>
       </section>
+      <Toaster />
     </main>
   );
 };

b373a6297164adb1e85eaf4e7398960d0457e040
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2024-11-17 20:21:33 +0200

    feat(files): implement file retrieval with user-specific queries
    - Added `createQueries` function to generate queries based on the current user's ID and email.
    - Integrated `createQueries` in `getFiles` to fetch files accessible by the current user.
    - Included error handling for cases where the current user is not found.
    - Added logging for debugging current user and query execution.
    
    TODO: Add support for search, sorting, and limits in the query logic.
    
    BUG : ownerId is not getting passed, I need to set it manually from appwrite

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -4,9 +4,10 @@ import { UploadFileProps } from "@/types";
 import { createAdminClient } from "@/lib/appwrite";
 import { InputFile } from "node-appwrite/file";
 import { appWriteConfig } from "@/lib/appwrite/config";
-import { ID } from "node-appwrite";
+import { ID, Query, Models } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "@/lib/actions/user.actions";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -55,5 +56,40 @@ export const uploadFile = async ({
     return parseStringify(newFile);
   } catch (error) {
     handleError(error, "Failed to upload files");
+  }
+};
+
+const createQueries = (currentUser: Models.Document) => {
+  const queries = [
+    Query.or([
+      Query.equal("owner", [currentUser.$id]),
+      Query.contains("users", [currentUser.email]),
+    ]),
+  ];
+  // TODO : Search, sort, limits...
+
+  return queries;
+};
+
+export const getFiles = async () => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser) throw new Error("User not found!");
+
+    const queries = createQueries(currentUser);
+    console.log({ currentUser, queries });
+
+    const files = await databases.listDocuments(
+      appWriteConfig.databaseId,
+      appWriteConfig.filesCollectionId,
+      queries,
+    );
+    console.log({ files });
+
+    return parseStringify(files);
+  } catch (error) {
+    handleError(error, "Failed to get files");
   }
 };

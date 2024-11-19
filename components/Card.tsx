4d1a0c2178dce7a4d026248b3f03f82cfe9fbb68
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2024-11-19 10:43:49 +0200

    feat: define ActionType interface with label, icon, and value properties
    Added a new ActionType interface that includes properties for label, icon, and value. This provides a structured way to represent actions with associated metadata, improving type safety and maintainability.

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Thumbnail from "@/components/Thumbnail";
 import { convertFileSize } from "@/lib/utils";
 import FormattedDateTime from "@/components/FormattedDateTime";
+import ActionDropDown from "@/components/ActionDropDown";
 
 const Card = ({ file }: { file: Models.Document }) => {
   return (
@@ -17,7 +18,7 @@ const Card = ({ file }: { file: Models.Document }) => {
           imageClassName="size-11"
         />
         <div className="flex flex-col items-end justify-between">
-          ActionsDropdown...
+          <ActionDropDown file={file} />
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
@@ -28,7 +29,6 @@ const Card = ({ file }: { file: Models.Document }) => {
           className="body-2 text-light-100"
         />
         <p className="caption line-clamp-1 text-light-200">
-          {" "}
           By : {file.owner.fullName}
         </p>
       </div>
